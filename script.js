8b100808f67f84fc14a1d7bd3653116f3b8fc287
Shahid <109452231+raza-shahi@users.noreply.github.com>
2026-01-05 08:27:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+    if(s.length == 0){
+        return s;
+    }
+    s=s.trim();
+    let splitedStr = s.split(" ");
+    return splitedStr[0];
 }
 
 // Do not change the code below
